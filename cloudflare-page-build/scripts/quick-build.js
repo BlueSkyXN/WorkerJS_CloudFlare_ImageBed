9d9a6ccba028bf7d185b6f757791ef8fe35f2cc0
BlueSkyXN <63384277+BlueSkyXN@users.noreply.github.com>
2024-03-27 15:24:48 +0800

    Base64抗爬虫

--- cloudflare-page-build/scripts/quick-build.js
+++ cloudflare-page-build/scripts/quick-build.js
@@ -21,13 +21,17 @@ fs.readFile(destPath, 'utf8', function (err, data) {
         return console.log(err);
     }
 
-    // 替换{{API_ENDPOINT}}占位符为环境变量的值，如果环境变量未设置，则使用默认值
+    // 获取API_ENDPOINT环境变量的值，如果未设置，则使用默认值
     const apiEndpoint = process.env.API_ENDPOINT || 'https://github.com/BlueSkyXN/WorkerJS_CloudFlare_ImageBed';
-    const result = data.replace(/{{API_ENDPOINT}}/g, apiEndpoint);
+    // 将API_ENDPOINT的值转换为Base64编码
+    const encodedApiEndpoint = Buffer.from(apiEndpoint).toString('base64');
+
+    // 在HTML文件内容中查找{{API_ENDPOINT_BASE64}}占位符，并用Base64编码后的API_ENDPOINT值替换它
+    const result = data.replace(/{{API_ENDPOINT_BASE64}}/g, encodedApiEndpoint);
 
     // 将修改后的内容写回文件
     fs.writeFile(destPath, result, 'utf8', function (err) {
          if (err) return console.log(err);
-         console.log('API_ENDPOINT已替换为', apiEndpoint,'并保存到:', destPath);
+         console.log('API_ENDPOINT已转换为Base64编码', encodedApiEndpoint, 'API_ENDPOINT已转换为', apiEndpoint, '并保存到:', destPath);
     });
 });
