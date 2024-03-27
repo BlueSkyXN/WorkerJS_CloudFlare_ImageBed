5cccf19fc3f923e5ad783e3b4f56d2afd02877ea
BlueSkyXN <63384277+BlueSkyXN@users.noreply.github.com>
2024-03-27 14:47:03 +0800

    20240327-1447

--- cloudflare-page-build/scripts/quick-build.js
+++ cloudflare-page-build/scripts/quick-build.js
@@ -2,22 +2,32 @@ const fs = require('fs');
 const path = require('path');
 
 // 设置源文件和目标文件的路径
-const srcDir = path.join(__dirname, '..', '..', 'cloudflare-page'); // cloudflare-page 目录的路径
-const srcFilePath = path.join(srcDir, 'Tools-TGPH.html'); // 源文件路径
-const destDir = path.join(__dirname, '..', '..', 'dist'); // 目标 dist 目录的路径
-const destToolsDir = path.join(destDir, 'tools'); // 目标 tools 目录的路径
-const destFilePath = path.join(destToolsDir, 'tgph.html'); // 目标文件路径
+const srcPath = path.join(__dirname, '..', '..', 'cloudflare-page', 'OneAPI-imgbed-MIX.html');
+const destDir = path.join(__dirname, '..', '..', 'dist');
+const destPath = path.join(destDir, 'index.html');
 
 // 确保目标目录存在
-if (!fs.existsSync(destToolsDir)) {
-    fs.mkdirSync(destToolsDir, { recursive: true });
+if (!fs.existsSync(destDir)) {
+    fs.mkdirSync(destDir, { recursive: true });
 }
 
-// 检查源文件是否存在
-if (fs.existsSync(srcFilePath)) {
-    // 复制并重命名文件
-    fs.copyFileSync(srcFilePath, destFilePath);
-    console.log(`文件 ${srcFilePath} 已成功复制并重命名到 ${destFilePath}`);
-} else {
-    console.log(`源文件 ${srcFilePath} 不存在。`);
-}
+// 复制并重命名文件
+fs.copyFileSync(srcPath, destPath);
+console.log('文件已成功复制并重命名到:', destPath);
+
+// 读取目标文件内容
+fs.readFile(destPath, 'utf8', function (err, data) {
+    if (err) {
+        return console.log(err);
+    }
+
+    // 替换{{API_ENDPOINT}}占位符为环境变量的值，如果环境变量未设置，则使用默认值
+    const apiEndpoint = process.env.API_ENDPOINT || 'https://github.com/BlueSkyXN/WorkerJS_CloudFlare_ImageBed';
+    const result = data.replace(/{{API_ENDPOINT}}/g, apiEndpoint);
+
+    // 将修改后的内容写回文件
+    fs.writeFile(destPath, result, 'utf8', function (err) {
+         if (err) return console.log(err);
+         console.log('API_ENDPOINT已替换，并保存到:', destPath);
+    });
+});
